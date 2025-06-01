c2f94e50aa0166d16960cbd59dd88a53d9f3f996
Eske Yoshinob <eske3g@gmail.com>
2025-06-01 23:31:47 +0900

    subdivSmoothSettingの対象を、グループにも拡大

--- extraConstructor/subdivSmoothSetting.py
+++ extraConstructor/subdivSmoothSetting.py
@@ -1,34 +1,37 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-r'''
-    @file     subdivSmoothSetting.py
-    @brief    モデルのsubudiv_setに対してsmoothMeshPreviewの処理を追加する
-    @class    ExtraConstructor : smoothMeshPreview処理を追加する
-    @date        2017/02/25 13:10[Eske](eske3g@gmail.com)
-    @update      2019/02/06 16:42[Eske](eske3g@gmail.com)
-    このソースの版権はEske Yoshinobにあります
-    無断転載、改ざん、無断使用は基本的に禁止しておりますので注意して下さい
-    このソースを使用して不具合や不利益等が生じても[Eske Yoshinob]
-    は一切責任を負いませんのであらかじめご了承ください
-'''
+# old_style:google style:google
+r"""
+    モデルのsubudiv_setに対してsmoothMeshPreviewの処理を追加する
+    
+    Dates:
+        date:2017/02/25 13:10[Eske](eske3g@gmail.com)
+        update:2025/06/01 23:21 Eske Yoshinob[eske3g@gmail.com]
+        
+    License:
+        Copyright 2017 Eske Yoshinob[eske3g@gmail.com] - All Rights Reserved
+        Unauthorized copying of this file, via any medium is strictly prohibited
+        Proprietary and confidential
+"""
 from gris3 import extraConstructor, node
 cmds = node.cmds
 
 class ExtraConstructor(extraConstructor.ExtraConstructor):
-    r'''
-        @brief       smoothMeshPreview処理を追加する
-        @inheritance extraConstructor.ExtraConstructor
-        @date        2017/02/25 13:10[Eske](eske3g@gmail.com)
-        @update      2019/02/06 16:42[Eske](eske3g@gmail.com)
-    '''
+    r"""
+        smoothMeshPreview処理を追加する
+    """
     SetName = 'subdiv_set'
     PreviewLevel = 0
     RenderSmoothLevel = 2
     def postProcess(self):
-        r'''
-            @brief  後処理時に実行。
-            @return None
-        '''
+        r"""
+            後処理時に実行。
+        """
         def setSubdivSetting(mesh):
+            r"""
+                Args:
+                    mesh (node.Mesh):
+            """
             print('Set subdivision setting : %s' % mesh)
             mesh('dsm', 2)
             mesh('lev', self.PreviewLevel)
@@ -39,10 +42,12 @@ class ExtraConstructor(extraConstructor.ExtraConstructor):
 
         model_grp = cst.modelGroup()
         extra_grp = [
-            getattr(model_grp, x)() for x in ('rigDataGroup', 'renderDataGroup')
+            getattr(model_grp, x)()
+            for x in ('rigDataGroup', 'renderDataGroup')
         ]
         children = [
-            x for x in model_grp.children(type='transform') if not x in extra_grp
+            x for x in model_grp.children(type='transform')
+            if not x in extra_grp
         ]
         all_meshes = [
             x for x in
@@ -61,6 +66,13 @@ class ExtraConstructor(extraConstructor.ExtraConstructor):
             mesh('lev', 0)
             mesh('uspr', True)
             mesh('rsl', self.RenderSmoothLevel)
-        for mesh in subdiv_nodes:
-            setSubdivSetting(mesh)
+        for sbdv_node in subdiv_nodes:
+            if sbdv_node.isType('mesh'):
+                meshs = [sbdv_node]
+            elif sbdv_node.isSubType('transform'):
+                meshs = sbdv_node.allChildren(type='mesh', ni=True)
+            else:
+                continue
+            for mesh in meshs:
+                setSubdivSetting(mesh)
         subdiv_set.delete()
